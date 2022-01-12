eeb2dcaffc099c516cbb2baed708319fd5b543f9
Atharva Deolalikar <atharvadeolalikar@gmail.com>
2022-01-12 22:14:09 +0530

    result fixed

--- src/Pages/Result.js
+++ src/Pages/Result.js
@@ -1,12 +1,20 @@
 import { useParams } from "react-router-dom";
+import Navbar from "../Components/Navbar";
 import resultImage from "../result.jpg";
 
 export default function Result() {
   const { examID } = useParams();
   console.log(examID);
   return (
-    <div className="resultContainer">
-      <img src={resultImage} className="embeddedImage" type="application/pdf" />
-    </div>
+    <>
+      <Navbar />
+      <div className="resultContainer">
+        <img
+          src={resultImage}
+          className="embeddedImage"
+          type="application/pdf"
+        />
+      </div>
+    </>
   );
 }
